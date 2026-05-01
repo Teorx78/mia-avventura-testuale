6a8242264cdbc5856d04e5ecac08c79c8372c5bc
Teorx78 <teorx78@gmail.com>
2026-05-01 17:05:19 +0200

    gear.py aggiunto

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import sys
 percorso_base = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(percorso_base, "py_functions"))
 
-# Immaginiamo che dentro py_functions tu crei un file chiamato 'motore.py'
+# Immaginiamo che dentro py_functions tu crei un file chiamato 'gear.py'
 # che conterrà la logica del loop che abbiamo visto prima.
 try:
-    from py_functions import motore
+    from py_functions import gear
 except ImportError:
-    print("ERRORE: Assicurati di aver creato la cartella 'py_functions' e il file 'motore.py' al suo interno.")
+    print("ERRORE: Assicurati di aver creato la cartella 'py_functions' e il file 'gear.py' al suo interno.")
     sys.exit(1)
 
 
@@ -18,13 +18,13 @@ def main():
     print("Caricamento risorse di gioco in corso...")
 
     # Qui potresti chiamare una funzione che carica i JSON dalle cartelle 'scenes', 'stats', 'npcs'
-    # database_scene = motore.carica_json(os.path.join(percorso_base, "scenes"))
+    # database_scene = gear.carica_json(os.path.join(percorso_base, "scenes"))
 
     print("Risorse caricate. Avvio avventura...\n")
     print("*" * 40)
 
     # Facciamo partire il loop principale del gioco
-    motore.avvia_gioco()
+    gear.avvia_gioco()
 
 
 if __name__ == "__main__":
